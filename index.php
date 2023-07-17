4f5205bd94aac2739d14bd5d462a2be869392b13
Giftlord <cleversongift@gmail.com>
2023-07-17 21:42:29 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <title>Coba git Remote</title>
 </head>
 <body>
-    <h1>Hello world!</p>
+    <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt ut veniam vel mollitia ducimus illum rerum eveniet quas eaque quibusdam aliquid voluptate reiciendis, suscipit unde qui quod assumenda enim.</p>
     
 </body>
 </html>
