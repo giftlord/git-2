420c805e18a275f31af0de9a301bd3622a015f07
Giftlord <cleversongift@gmail.com>
2023-07-17 21:49:00 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt ut veniam vel mollitia ducimus illum rerum eveniet quas eaque quibusdam aliquid voluptate reiciendis, suscipit unde qui quod assumenda enim.</p>
-    
+<ul>
+    <li>satu</li>
+    <li>dua</li>
+</ul>    
 </body>
 </html>
